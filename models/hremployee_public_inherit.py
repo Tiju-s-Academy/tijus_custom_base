7c4f1141a76e0bd8ca003778dc6dc27ac16b2ee6
Adil <adilpk123@gmail.com>
2025-01-03 11:40:26 +0530

    Voxbay Agent outgoing Number added

--- models/hremployee_public_inherit.py
+++ models/hremployee_public_inherit.py
@@ -8,4 +8,4 @@ class HrEmployeePublicInherit(models.Model):
     agent_number = fields.Char('Voxbay Agent Number')
     related_joinee = fields.Many2one('employee.joining.form',string='Related Joinee')
     biometric_no = fields.Char(string='Biometric Code')
-
+    voxbay_agent_number_outgoing = fields.Char('Voxbay Agent outgoing Number')
