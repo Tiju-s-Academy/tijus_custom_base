165b0f1fc4b8c7542e4eeff6beffa9ac75340be4
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-10 16:44:22 +0530

    ..

--- models/project_task.py
+++ models/project_task.py
@@ -1,32 +1,16 @@
 from odoo import models, fields, api
 
-class ProjectTask(models.Model):
-    _inherit = 'project.task'
-
-    task_status = fields.Selection([
-        ('normal', 'Normal'),
-        ('hold', 'Hold'),
-        ('pending', 'Pending')
-    ], string='Task Status', default='normal', tracking=True)
-
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
     _order = 'sequence, id'
 
-    @api.model
-    def _get_state_selection(self):
-        # Get original selection
-        selections = self.env['project.task.type']._fields['state'].selection
-        if not selections:
-            selections = []
-        # Add our new states
-        return selections + [
-            ('05_hold', 'Hold'),
-            ('06_pending', 'Pending')
-        ]
-
-    state = fields.Selection(
-        selection=_get_state_selection,
-        string='State',
-        default='01_in_progress'
-    )
+    state = fields.Selection([
+        ('01_in_progress', 'In Progress'),
+        ('02_changes_requested', 'Changes Requested'),
+        ('03_approved', 'Approved'),
+        ('04_waiting_normal', 'Waiting'),
+        ('05_hold', 'Hold'),
+        ('06_pending', 'Pending'),
+        ('1_done', 'Done'),
+        ('1_canceled', 'Canceled')
+    ], string='State', default='01_in_progress')
