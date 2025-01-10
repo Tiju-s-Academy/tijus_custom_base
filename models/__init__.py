8f016f220d2167e30d665f09d37999b20743c293
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-10 16:02:55 +0530

    Revert "Add new task states and update project task model for enhanced functionality"
    This reverts commit 3f4fb2a04512439dd75f30b4e52c15e4308584c0.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,10 @@
 from . import employee_branch
 from . import hremployee_public_inherit
 from . import hr_employee_inherit
+<<<<<<< HEAD
 from . import project_task
+=======
+>>>>>>> parent of 3f4fb2a (Add new task states and update project task model for enhanced functionality)
 
 
 
--- models/project_task.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ProjectTaskType(models.Model):
-    _inherit = 'project.task.type'
-
-    state = fields.Selection(selection_add=[
-        ('hold', 'Hold'),
-        ('pending', 'Pending')
-    ])
