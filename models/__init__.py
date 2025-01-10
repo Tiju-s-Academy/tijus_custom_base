9957fb9a6ea92cd151b668fa1b2329698dfb6020
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-10 16:05:11 +0530

    Revert "Add new task states and update project task model for enhanced functionality"
    This reverts commit 3f4fb2a04512439dd75f30b4e52c15e4308584c0.

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,7 @@
 from . import employee_branch
 from . import hremployee_public_inherit
 from . import hr_employee_inherit
-<<<<<<< HEAD
-<<<<<<< HEAD
 from . import project_task
-=======
->>>>>>> parent of 3f4fb2a (Add new task states and update project task model for enhanced functionality)
-=======
->>>>>>> parent of 3f4fb2a (Add new task states and update project task model for enhanced functionality)
-
-
 
 
 
