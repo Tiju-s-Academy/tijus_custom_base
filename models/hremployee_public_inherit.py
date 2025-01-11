8429c0a21d1c1a2419391584995d09173f196d9a
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-11 11:06:09 +0530

    Update agent_number field in HrEmployeePublicInherit model to remove related attribute for improved data integrity

--- models/hremployee_public_inherit.py
+++ models/hremployee_public_inherit.py
@@ -4,6 +4,6 @@ class HrEmployeePublicInherit(models.Model):
     _inherit = 'hr.employee.public'
 
     branch_id = fields.Many2one('employee.branch', string='Branch', related='employee_id.branch_id', readonly=False)
-    agent_number = fields.Char('Voxbay Agent Number', related='employee_id.agent_number', readonly=False)
+    agent_number = fields.Char('Voxbay Agent Number', readonly=False)
     related_joinee = fields.Many2one('employee.joining.form', string='Related Joinee', related='employee_id.related_joinee', readonly=False)
     biometric_no = fields.Char(string='Biometric Code', related='employee_id.biometric_no', readonly=False)
