3125b04f7979640b6de8c741cf165aee994bebfc
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-11 10:54:33 +0530

    Add hremployee_public_inherit model and update related fields in HrEmployeePublicInherit for improved data management

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from . import employee_branch
 from . import hr_employee
 from . import project_task
+from . import hremployee_public_inherit  # Add this line
--- models/hremployee_public_inherit.py
+++ models/hremployee_public_inherit.py
@@ -1,11 +1,9 @@
-from odoo import fields,models
-
+from odoo import fields, models
 
 class HrEmployeePublicInherit(models.Model):
     _inherit = 'hr.employee.public'
 
-    branch_id = fields.Many2one('employee.branch', string='Branch')
-    agent_number = fields.Char('Voxbay Agent Number')
-    related_joinee = fields.Many2one('employee.joining.form',string='Related Joinee')
-    biometric_no = fields.Char(string='Biometric Code')
-    # voxbay_agent_number_outgoing = fields.Char('Voxbay Agent outgoing Number')
+    branch_id = fields.Many2one('employee.branch', string='Branch', related='employee_id.branch_id', readonly=False)
+    agent_number = fields.Char('Voxbay Agent Number', related='employee_id.agent_number', readonly=False)
+    related_joinee = fields.Many2one('employee.joining.form', string='Related Joinee', related='employee_id.related_joinee', readonly=False)
+    biometric_no = fields.Char(string='Biometric Code', related='employee_id.biometric_no', readonly=False)
