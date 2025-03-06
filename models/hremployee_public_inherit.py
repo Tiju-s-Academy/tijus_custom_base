591134f85dd7b6806c959500cf5369a738e618a7
Adil <adilpk123@gmail.com>
2025-03-06 09:38:28 +0530

    added new fields in hr employee public

--- models/hremployee_public_inherit.py
+++ models/hremployee_public_inherit.py
@@ -8,3 +8,26 @@ class HrEmployeePublicInherit(models.Model):
     related_joinee = fields.Many2one('employee.joining.form', string='Related Joinee', related='employee_id.related_joinee', readonly=False)
     biometric_no = fields.Char(string='Biometric Code', related='employee_id.biometric_no', readonly=False)
     # voxbay_agent_number_outgoing = fields.Char(string="Voxbay Agent Number Outgoing", readonly=False)
+    
+    blood_group = fields.Char(string='Blood Group')
+    emergency_contact_person_relationship = fields.Char(string='Emergency Contact Person Relationship')
+    emergency_contact_person_email = fields.Char(string='Emergency Contact Person Email')
+    emergency_contact_person_correspondence_address = fields.Char(
+        string='Emergency Contact Person Correspondence Address')
+
+    bank_name = fields.Char(string='Bank Name')
+    branch_bank = fields.Char(string='Branch')
+    ifsc_code = fields.Char(string='IFSC Code')
+
+    spouse_name = fields.Char(string='Spouse Name')
+
+    date_of_joining = fields.Date(string='Date Of Joining')
+
+    previous_employment_company_name = fields.Char(string='Previous Employment - Company Name')
+    previous_employment_company_location = fields.Char(string='Previous Employment - Company Location')
+    previous_employment_company_designation = fields.Char(string='Previous Employment - Company Designation')
+    total_years_of_experience = fields.Integer(
+        string='Total Years Of Experience')
+
+    aadhar_card_number = fields.Char(string='Aadhar Card Number')
+    pan_card_number = fields.Char(string='Pan Card Number')
